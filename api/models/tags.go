22939d8fe026ee24b38cbb7b06feb827077387fc
Yann Hulot <yannhulot@geniesolutions.com.au>
2019-11-12 09:07:11 +1000

    remove commented code

--- api/models/tags.go
+++ api/models/tags.go
@@ -4,7 +4,6 @@ package models
 // ie: small, cute
 type Tag struct {
 	ID uint64 `gorm:"primary_key;not null;unique" json:"id"`
-	// TagID uint64 `gorm:"primary_key" json:"id"`
 	Name  string `json:"name"`
 	PetID uint64 `json:"-"`
 }
